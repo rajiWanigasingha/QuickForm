9433d9ea2d6a12b8988434eeb2b2b0968874eb25
rajindawanigasingha <rajindawanigasingha@gmail.com>
2026-02-04 09:32:51 +0530

    Update package metadata and gitignore
    - Renamed package to `@rajinda_wanigasingha/quickform` in `package.json`.
    - Added `forms` keyword to package metadata.
    - Updated `.gitignore` to include `.npmrc`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,17 @@
-// Reexport your entry components here
+// Export all component states
+export { TextState } from '$lib/components/Text/TextState.svelte.js';
+export { NumberState } from '$lib/components/Numbers/NumberState.svelte.js';
+export { BooleanState } from '$lib/components/Boolean/BooleanState.svelte.js';
+export { ChoicesState } from '$lib/components/Choices/ChoicesState.svelte.js';
+export { SelectState } from '$lib/components/Select/SelectState.svelte.js';
+
+// Export all validations
+export { TextValidation } from '$lib/components/Text/Text.validation.js';
+export { NumberValidation } from '$lib/components/Numbers/Number.validation.js';
+export { BooleanValidation } from '$lib/components/Boolean/Boolean.validation.js';
+
+// Export QuickFormBuilder component
+export { default as QuickFormBuilder } from '$lib/compileFormSchema/QuickFormBuilder.svelte';
+
+// Export types
+export type * from '$lib/types/schema.js';
